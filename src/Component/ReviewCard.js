9a30bcdac9cef0f5bb2607db2dc515ddf67f5144
Bishesh <bishesh.sunam@gmail.com>
2021-08-09 01:09:50 +1200

    added our team section on home page

--- src/Component/ReviewCard.js
+++ src/Component/ReviewCard.js
@@ -2,17 +2,18 @@ import React from "react";
 import "./ReviewCard.css";
 import { Avatar } from "@material-ui/core";
 
-function ReviewCard({ imgURL, fname, lname, title, description }) {
+function ReviewCard({ imgURL, fname, lname, title, description, color }) {
   return (
     <div className="reviewCard">
-      <div className="reviewCard__top">
+      <div className="reviewCard__top" style={{backgroundColor: color}}></div>
+      <div className="reviewCard__middle">
         <Avatar style={{ height: "70px", width: "70px" }} src={imgURL}></Avatar>
+      </div>
+      <div className="reviewCard__bottom">
         <div className="reviewCard__top--name">
           <p>{fname}</p>
           <p>{lname}</p>
         </div>
-      </div>
-      <div className="reviewCard__bottom">
         <h4>{title}</h4>
         <p>{description}</p>
       </div>
