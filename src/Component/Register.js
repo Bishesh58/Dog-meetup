f536952661b1f994bad30e0ded8e66c0a89c7d0f
Bishesh <bishesh.sunam@gmail.com>
2021-08-11 23:25:59 +1200

    added Login, Register & contact UI

--- src/Component/Register.js
+++ src/Component/Register.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import './Register.css';
+import React from "react";
+import "./Register.css";
+import imgRegister from "../img/imgRegister.jpeg";
 
 function Register() {
-    return (
-        <div className="register">
-            you are on register page
+  return (
+    <div className="register">
+      <div className="register__container">
+        <div className="register__container--left">
+          <img src={imgRegister} alt="" />
         </div>
-    )
+        <div className="register__container--right">
+          <form>
+            <input placeholder="First Name" type="fname" />
+            <input placeholder="Last Name" type="lname" />
+            <input placeholder="Email" type="email" />
+            <input placeholder="Password" type="password" />
+            <input placeholder="Address" type="address" />
+            <input placeholder="Dog name" type="dname" />
+            <input placeholder="Dog breed" type="dbreed" />
+            <button type="submit" onClick="">
+              Sign up
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
