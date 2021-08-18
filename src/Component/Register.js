784a7af3073c36749624be5f093a7ae343aaa0ce
Bishesh <bishesh.sunam@gmail.com>
2021-08-18 15:32:37 +1200

    added auto complete location on Registration page

--- src/Component/Register.js
+++ src/Component/Register.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState, useRef, useCallback } from "react";
 import "./Register.css";
 import imgRegister from "../img/imgRegister.jpeg";
+import MapboxAutocomplete from "react-mapbox-autocomplete";
 
 function Register() {
+  const getAddress=(result, lat, lng, text)=>{
+    console.log(result);
+  }
   return (
     <div className="register">
       <div className="register__container">
@@ -15,9 +19,16 @@ function Register() {
             <input placeholder="Last Name" type="lname" />
             <input placeholder="Email" type="email" />
             <input placeholder="Password" type="password" />
-            <input placeholder="Address" type="address" />
             <input placeholder="Dog name" type="dname" />
             <input placeholder="Dog breed" type="dbreed" />
+            <label className="lblAddress">Address</label>
+            <MapboxAutocomplete
+              publicKey={process.env.REACT_APP_MAPBOX}
+              onSuggestionSelect={getAddress}
+              country='nz'
+              resetSearch={false}
+            />
+
             <button type="submit" onClick="">
               Sign up
             </button>
