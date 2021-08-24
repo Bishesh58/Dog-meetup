94266aec3ee7b909b16a5a4413329c300d6a69a5
Bishesh <bishesh.sunam@gmail.com>
2021-08-25 02:03:48 +1200

    ready for design deployment

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,12 +1,12 @@
-import { loginStart, loginSuccess, loginError } from "./userSlice";
-import axios from 'axios';
+// import { loginStart, loginSuccess, loginError } from "./userSlice";
+// import axios from 'axios';
 
-export const loginCall = async(user, dispatch)=>{
-    dispatch(loginStart());
-    try {
-        const res = await axios.post("http:localhost:5000//api/users/login", user)
-        dispatch(loginSuccess());
-    } catch (error) {
-        dispatch(loginError())
-    }
-}
+// export const loginCall = async(user, dispatch)=>{
+//     dispatch(loginStart());
+//     try {
+//         const res = await axios.post("http:localhost:5000//api/users/login", user)
+//         dispatch(loginSuccess());
+//     } catch (error) {
+//         dispatch(loginError())
+//     }
+// }
